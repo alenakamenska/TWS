0a8da964b582315b6eb48be1227148bb133b4992
Alena Kamenska <alenka.kamenska@seznam.cz>
2025-02-21 09:46:04 +0100

    responzivita

--- scripts/index.js
+++ scripts/index.js
@@ -27,13 +27,5 @@ document.addEventListener("DOMContentLoaded", () => {
             sklizen.value = urodnost;
         }
     });
-    document.addEventListener("scroll", function() {
-        const header = document.querySelector("header");
-        if (window.scrollY > 50) {
-            header.classList.add("hidden");
-        } else {
-            header.classList.remove("hidden");
-        }
-    });
     
 });
