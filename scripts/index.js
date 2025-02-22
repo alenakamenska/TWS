aa6c3c8fe384b5531aaeab0ed5753db8166b6ac9
Alena Kamenska <alenka.kamenska@seznam.cz>
2025-02-22 12:14:48 +0100

    rostliny

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const sklizen = document.getElementById("sklizen");
 
     form.addEventListener("submit", (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
 
         let zasazeno = parseInt(zasazenoInput.value);
         let sklizeno = parseInt(sklizenoInput.value);
@@ -14,18 +14,17 @@ document.addEventListener("DOMContentLoaded", () => {
         if (isNaN(zasazeno) || isNaN(sklizeno) || zasazeno <= 0) {
             vysledek.textContent = "Neplatný vstup!";
             vysledek.style.color = "red";
-        } if(zasazeno < sklizeno){
-            vysledek.textContent = "Nelze sklidit více než je zasazeno";
+        } else if (sklizeno > zasazeno) {
+            vysledek.textContent = "Nelze sklidit více, než je zasazeno!";
             vysledek.style.color = "red";
-        } if(zasazeno <= 0 || sklizeno < 0){
-            vysledek.textContent = "Hodnoty nemůžou být záporné";
+        } else if (zasazeno <= 0 || sklizeno < 0) {
+            vysledek.textContent = "Hodnoty nemůžou být záporné.";
             vysledek.style.color = "red";
-        }else {
+        } else {
             let urodnost = (sklizeno / zasazeno) * 100;
             vysledek.textContent = `Úrodnost: ${urodnost.toFixed(2)} %`;
             vysledek.style.color = "#2e6b31";
             sklizen.value = urodnost;
         }
     });
-    
 });
