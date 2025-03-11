5983a70fcb550f4fecc772304a4b3d41bad6a67b
Alena Kamenska <alenka.kamenska@seznam.cz>
2025-03-11 11:35:47 +0100

    uprava

--- scripts/dark_mode.js
+++ scripts/dark_mode.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const toggleButton = document.getElementById("dark-mode-toggle");
+    const btn = document.getElementById("btn-mode");
     const body = document.body;
     if (localStorage.getItem("dark-mode") === "enabled") {
         body.classList.add("dark-mode");
-        toggleButton.textContent = "☀️ Light Mode";
+        btn.textContent = "☀️ Light Mode";
     }
 
-    toggleButton.addEventListener("click", function() {
+    btn.addEventListener("click", function() {
         body.classList.toggle("dark-mode");
 
         if (body.classList.contains("dark-mode")) {
             localStorage.setItem("dark-mode", "enabled");
-            toggleButton.textContent = "☀️ Light Mode";
+            btn.textContent = "☀️ Light Mode";
         } else {
             localStorage.setItem("dark-mode", "disabled");
-            toggleButton.textContent = "🌙 Dark Mode";
+            btn.textContent = "🌙 Dark Mode";
         }
     });
 });
